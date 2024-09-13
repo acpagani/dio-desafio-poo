0d644402dada5fa8c180bb4331dd957af9f9cc85
Arthur Pagani <112992377+acpagani@users.noreply.github.com>
2024-09-13 12:56:07 -0300

    finalização do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+﻿using System.Transactions;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia: ");
+Smartphone nokia = new Nokia(numero: "119353534534", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("iPhone");
+Smartphone iphone = new Iphone(numero: "1192342342", modelo: "16 pro max", imei: "22222222", memoria: 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
